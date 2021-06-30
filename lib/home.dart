1a4b718824a36edf231804875cd39395514f8689
plagaz <plagaz66@gmail.com>
2021-06-29 21:53:11 -0400

    Clase 5 (01/06/2021) Widgets Stack, Column, Row, Container, Text y IconURL

---
+++ lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'description_place.dart';
+
+class MyHome extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    final descriptionPlace = Container(
+      margin: EdgeInsets.only(
+          top:250,
+          left:30,
+          right:30
+      ),
+      child: DescriptionPlace(),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "My Places"
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          descriptionPlace
+        ],
+      )
+    );
+  }
+}
